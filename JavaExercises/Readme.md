ae0e211b7403b9a06055264597f99fae11406f8a
ivorycirrus <ivorycirrus@ivorycirrus-desktop>
2019-02-10 01:11:56 +0900

    add step3:ex01 - 369 game

--- JavaExercises/Readme.md
+++ JavaExercises/Readme.md
@@ -3,3 +3,8 @@
 ### Step 1
 * [[Ex01] 3과 5의 배수의 합](src/basic/step1/ex01)
 * [[Ex02] 윤년 계산](src/basic/step1/ex02)
+
+### Step 2
+
+### Step 3
+* [[Ex01] 369 게임](src/basic/step3/ex01)
---
+++ JavaExercises/src/basic/step3/ex01/Main.java
@@ -0,0 +1,24 @@
+package basic.step3.ex01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int N=1; // Num of players
+		int S=1; // Seat
+
+		call(N, S);
+	}
+	
+	public static void call(int N, int S) {
+		//---------------------------------------
+		// TODO : Write code below
+		
+		for(int i = 0 ; i < 5 ; i++) {			
+			System.out.println(i);
+		}
+		
+		//---------------------------------------
+	}
+
+}

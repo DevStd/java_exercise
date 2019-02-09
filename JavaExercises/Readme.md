c73b27b7a053780e136afed232482e76430ddf4c
ivorycirrus <ivorycirrus@ivorycirrus-desktop>
2019-02-10 00:17:42 +0900

    add exercise : ex02 - leap year

--- JavaExercises/Readme.md
+++ JavaExercises/Readme.md
@@ -2,3 +2,4 @@
 
 ### Step 1
 * [[Ex01] 3과 5의 배수의 합](src/basic/step1/ex01)
+* [[Ex02] 윤년 계산](src/basic/step1/ex02)
---
+++ JavaExercises/src/basic/step1/ex02/Main.java
@@ -0,0 +1,20 @@
+package basic.step1.ex02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int year = 2019;
+		
+		printYear(year);
+	}
+	
+	public static void printYear(int year) {
+		//---------------------------------------
+		// TODO : Write code below
+		
+		System.out.println(year+"년은 평년입니다.");
+		
+		//---------------------------------------
+	}
+
+}

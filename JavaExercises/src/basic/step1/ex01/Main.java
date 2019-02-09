ccee6672c1a774396a4b4febb22e4856ee4d8c5d
Pilmo Kang <ivorycirrus@gmail.com>
2019-02-09 17:52:50 +0900

    add exercise : ex01 - summation

---
+++ JavaExercises/src/basic/step1/ex01/Main.java
@@ -0,0 +1,23 @@
+package basic.step1.ex01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int from = 1;
+		int to = 20;
+		
+		int sum = findSum(from, to);
+
+		System.out.println("Sum : "+sum);
+	}
+	
+	public static int findSum(int from, int to) {
+		int sum = 0;
+		//---------------------------------------
+		// TODO : Write code below
+		
+		//---------------------------------------
+		return sum;
+	}
+
+}
